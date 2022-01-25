d75e5d6c5879cdb6ab8754480f87bede7499af8e
leqiang-deltafi <96594219+leqiang-deltafi@users.noreply.github.com>
2022-01-24 17:34:41 -0800

    fix for rewards (#126)

--- src/lib/state/rewards.ts
+++ src/lib/state/rewards.ts
@@ -3,6 +3,7 @@ import { bool, u64 } from 'utils/layout'
 
 export interface Rewards {
   isInitialized: boolean
+  threshold: bigint
   tradeRewardNumerator: bigint
   tradeRewardDenominator: bigint
   tradeRewardCap: bigint
@@ -13,6 +14,7 @@ export const RewardsLayout = (property = 'rewards') =>
   struct<Rewards>(
     [
       bool('isInitialized'),
+      u64('threshold'),
       u64('tradeRewardNumerator'),
       u64('tradeRewardDenominator'),
       u64('tradeRewardCap'),
