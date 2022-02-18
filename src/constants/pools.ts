17a115368595569766911cc0f55eb3a611c541cd
leqiang-deltafi <leqiang@deltafi.ai>
2022-02-18 15:42:43 -0800

    update

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -13,15 +13,15 @@ export interface PoolSchema {
 export const pools: PoolSchema[] = [
   {
     name: 'SOL-USDC',
-    address: new PublicKey('Ac4JzVzC5WGU7froEinPBufRuXW29eBX9avDwhd4RiWQ'), 
-    mintAddress: new PublicKey('FmLx5gR9Zkyu8hhAghg2Ps9Ypt44PVpxpPEHwNWS9p42'), 
+    address: new PublicKey('Ac4JzVzC5WGU7froEinPBufRuXW29eBX9avDwhd4RiWQ'),
+    mintAddress: new PublicKey('FmLx5gR9Zkyu8hhAghg2Ps9Ypt44PVpxpPEHwNWS9p42'),
     base: 'SOL',
     quote: 'USDC',
   },
   {
     name: 'SRM-USDC',
-    address: new PublicKey('Avkrs6DM5U7rgFtnzie7Fnux4TfEj911nJM3kSETh2Vb'), 
-    mintAddress: new PublicKey('HxbHSeDZiLv2AhyGmUbnsxdGVJQG1s2Fz6AXxTDxtE5a'), 
+    address: new PublicKey('Avkrs6DM5U7rgFtnzie7Fnux4TfEj911nJM3kSETh2Vb'),
+    mintAddress: new PublicKey('HxbHSeDZiLv2AhyGmUbnsxdGVJQG1s2Fz6AXxTDxtE5a'),
     base: 'SRM',
     quote: 'USDC',
   },
