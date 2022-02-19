c507ac991240a0b33a8cb8982a1a88a42c31f1bc
leqiang-deltafi <leqiang@deltafi.ai>
2022-02-18 20:47:01 -0800

    remove commented code

--- src/constants/farm.ts
+++ src/constants/farm.ts
@@ -17,21 +17,3 @@ export const farmPools: FarmPoolSchema[] = POOLS.map(({name, swap, farm}) => ({
   base: name.split("-")[0], 
   quote: name.split("-")[1]
 }))
-
-// export const farmPools: FarmPoolSchema[] = [
-//   {
-//     name: 'SRM-USDC',
-//     address: new PublicKey('CD6peeUopKcKakgQs2QbN464vCr2oztDAMGgMWqrmjXA'),
-//     poolAddress: new PublicKey('9D2YvHZfNuDq5QTohtSZy4CWCRxYnow9ZZtBMswHuJ7h'),
-//     base: 'SRM',
-//     quote: 'USDC',
-//   },
-  
-//   {
-//     name: 'SOL-SRM',
-//     address: new PublicKey('Co6WBZSk46w3DQskKfzHeH4icdddG6nC48hSSD56r4qZ'),
-//     poolAddress: new PublicKey('C6nWZRHSHva5wdtgoYLmZF8zanVvcsAedUqwstzp699D'),
-//     base: 'SOL',
-//     quote: 'SRM',
-//   },
-// ]
