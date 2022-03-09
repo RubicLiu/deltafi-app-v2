e080f019364a1321f3cc13371073ab615ac16e0d
ypeng-deltafi <ypeng@deltafi.ai>
2022-03-09 10:27:35 -0800

    Remove pyth product data fetch (#259)

--- src/states/PythState.ts
+++ src/states/PythState.ts
@@ -1,6 +1,6 @@
 import { createReducer, createAsyncThunk } from "@reduxjs/toolkit";
 import { PublicKey, Connection } from "@solana/web3.js";
-import { PriceData, ProductData, parseProductData, parsePriceData } from "@pythnetwork/client";
+import { PriceData, ProductData, parsePriceData } from "@pythnetwork/client";
 import BigNumber from "bignumber.js";
 
 import { deployConfig } from "constants/deployConfig";
@@ -46,22 +46,16 @@ export const pythReducer = createReducer(initialState, (builder) => {
 
 async function getPythDataList(connection: Connection) {
   const tokenInfo = deployConfig.tokenInfo;
-  const pythProductKeys = tokenInfo.map(({ pyth }) => new PublicKey(pyth.product));
-  const productsInfos = await getMultipleAccounts(connection, pythProductKeys, "confirmed");
-
   const pythPriceKeys = tokenInfo.map(({ pyth }) => new PublicKey(pyth.price));
   const priceInfos = await getMultipleAccounts(connection, pythPriceKeys, "confirmed");
-
   const pythDataList = [];
-  for (let i = 0; i < productsInfos.keys.length; i++) {
-    const productData = parseProductData(productsInfos.array[i].data as Buffer);
-    const priceKey = productData.priceAccountKey;
+  for (let i = 0; i < priceInfos.keys.length; i++) {
+    const priceKey = priceInfos.keys[i];
     const priceData = parsePriceData(priceInfos.array[i].data as Buffer);
     const symbol = tokenInfo[i].symbol;
     pythDataList.push({
       priceKey,
       symbol,
-      productData,
       priceData,
     });
   }
