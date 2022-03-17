3167d53e7dec84c55bb7f52b29613e93b1b63b83
ypeng-deltafi <ypeng@deltafi.ai>
2022-03-17 15:03:09 -0700

    Remove unused util functions (#300)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
-import { Keypair } from "@solana/web3.js";
 import { PoolInfo } from "providers/types";
 
 export function sleep(ms: number): Promise<void> {
@@ -14,33 +13,6 @@ export function reportError(err: unknown, context: string) {
 export function isLocalHost() {
   return window.location.hostname === "localhost";
 }
-
-export const getLocalStorageKeypair = (key: string): Keypair => {
-  const base64Keypair = window.localStorage.getItem(key);
-  if (base64Keypair) {
-    return Keypair.fromSecretKey(Uint8Array.from(atob(base64Keypair), (c) => c.charCodeAt(0)));
-  } else {
-    const account = Keypair.generate();
-    window.localStorage.setItem(key, Buffer.from(account.secretKey).toString("base64"));
-    return account;
-  }
-};
-
-const SPLIT = ((): number => {
-  const split = parseInt(new URLSearchParams(window.location.search).get("split") || "");
-  if (!isNaN(split)) {
-    return Math.min(split, 12);
-  }
-  return 4;
-})();
-
-export const FEE_PAYERS = (() => {
-  const accounts = [];
-  for (let i = 0; i < SPLIT; i++) {
-    accounts.push(getLocalStorageKeypair(`feePayerKey${i + 1}`));
-  }
-  return accounts;
-})();
 
 export function scheduleWithInterval(func: () => void, delay: number) {
   func();
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,18 +5,6 @@ export const convertDollar = (value) => {
   return "USD " + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
-export function isValidPublicKey(key) {
-  if (!key) {
-    return false;
-  }
-  try {
-    new PublicKey(key);
-    return true;
-  } catch {
-    return false;
-  }
-}
-
 export function formatPubkey(key: PublicKey) {
   if (key) {
     const keyStr = key.toBase58();
@@ -33,32 +21,6 @@ export function fixedNumber(
     return "";
   }
   return Number(number).toFixed(maxDecimalPlace);
-}
-
-export async function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-export const percentFormat = new Intl.NumberFormat(undefined, {
-  style: "percent",
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 2,
-});
-
-export function floorToDecimal(value: number, decimals: number | undefined | null) {
-  return decimals ? Math.floor(value * 10 ** decimals) / 10 ** decimals : Math.floor(value);
-}
-
-export function roundToDecimal(value: number, decimals: number | undefined | null) {
-  return decimals ? Math.round(value * 10 ** decimals) / 10 ** decimals : value;
-}
-
-export function getDecimalCount(value): number {
-  if (!isNaN(value) && Math.floor(value) !== value && value.toString().includes("."))
-    return value.toString().split(".")[1].length || 0;
-  if (!isNaN(value) && Math.floor(value) !== value && value.toString().includes("e"))
-    return parseInt(value.toString().split("e-")[1] || "0");
-  return 0;
 }
 
 const localStorageListeners = {};
@@ -116,78 +78,6 @@ export function useLocalStorageState<T = any>(
     (newState) => setStringState(JSON.stringify(newState)),
   ];
 }
-
-export function useEffectAfterTimeout(effect, timeout) {
-  useEffect(() => {
-    const handle = setTimeout(effect, timeout);
-    return () => clearTimeout(handle);
-  });
-}
-
-export function useListener(emitter, eventName) {
-  const [, forceUpdate] = useState(0);
-  useEffect(() => {
-    const listener = () => forceUpdate((i) => i + 1);
-    emitter.on(eventName, listener);
-    return () => emitter.removeListener(eventName, listener);
-  }, [emitter, eventName]);
-}
-
-export function abbreviateAddress(address: PublicKey, size = 4) {
-  const base58 = address.toBase58();
-  return base58.slice(0, size) + "…" + base58.slice(-size);
-}
-
-export function isEqual(obj1, obj2, keys) {
-  if (!keys && Object.keys(obj1).length !== Object.keys(obj2).length) {
-    return false;
-  }
-  keys = keys || Object.keys(obj1);
-  for (const k of keys) {
-    if (obj1[k] !== obj2[k]) {
-      // shallow comparison
-      return false;
-    }
-  }
-  return true;
-}
-
-export function flatten(obj, { prefix = "", restrictTo }) {
-  let restrict = restrictTo;
-  if (restrict) {
-    restrict = restrict.filter((k) => obj.hasOwnProperty(k));
-  }
-  const result = {};
-  (function recurse(obj, current, keys) {
-    (keys || Object.keys(obj)).forEach((key) => {
-      const value = obj[key];
-      const newKey = current ? current + "." + key : key; // joined key with dot
-      if (value && typeof value === "object") {
-        // @ts-ignore
-        recurse(value, newKey); // nested object
-      } else {
-        result[newKey] = value;
-      }
-    });
-  })(obj, prefix, restrict);
-  return result;
-}
-
-export const useOnClickOutside = (ref, handler) => {
-  useEffect(() => {
-    const listener = (event) => {
-      if (!ref.current || ref.current.contains(event.target)) {
-        return;
-      }
-      handler(event);
-    };
-    document.addEventListener("mousedown", listener);
-
-    return () => {
-      document.removeEventListener("mousedown", listener);
-    };
-  }, [ref, handler]);
-};
 
 export function chunks<T>(array: T[], size: number): T[][] {
   return Array.apply<number, T[], T[][]>(0, new Array(Math.ceil(array.length / size))).map(
