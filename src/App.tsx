49b131336befe8d10a129df120655bd83c7d6076
ypeng-deltafi <ypeng@deltafi.ai>
2022-04-28 12:11:35 -0700

    [PROD-645] Add pyth and token v2 redux state (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,8 @@ import { fetchSwapsV2Thunk } from "states/v2/swapV2State";
 import { fetchFarmsV2Thunk } from "states/v2/farmV2State";
 import { fetchLiquidityProvidersV2Thunk } from "states/v2/liqudityProviderV2State";
 import { fetchUserV2Thunk } from "states/v2/userV2State";
+import { fetchPythDataV2Thunk } from "states/v2/pythV2State";
+import { fetchTokenAccountsV2Thunk } from "states/v2/tokenV2State";
 
 // Amplify.configure(awsconfig)
 // Analytics.autoTrack('event', {
@@ -80,7 +82,10 @@ const App: React.FC = () => {
     const connection = new web3.Connection(clustApiUrl, "confirmed");
     dispatch(fetchSwapsV2Thunk({ connection, walletAddress }));
     dispatch(fetchFarmsV2Thunk({ connection, walletAddress }));
+    dispatch(fetchPythDataV2Thunk(connection));
+
     if (walletAddress != null) {
+      dispatch(fetchTokenAccountsV2Thunk({ connection, walletAddress }));
       dispatch(fetchLiquidityProvidersV2Thunk({ connection, walletAddress }));
       dispatch(fetchUserV2Thunk({ connection, walletAddress }));
     }
