cce2e2e36213ec1fec7082269456427c4b59304d
leqiang-deltafi <leqiang@deltafi.ai>
2022-04-29 10:32:03 -0700

    x

--- src/lib/calc/calculation.ts
+++ src/lib/calc/calculation.ts
@@ -8,6 +8,14 @@ function BigNumberWithConfig(
   return new BigNumber(val);
 }
 
+/**
+ * calculate out amount from v2 g(m) curve, with slope=1 case. the formula is:
+ * - token_b_output = b * (1 - (a / (a + m))^(P * A / B))
+ * - a = current_reserve_a, b = current_reserve_b (current token reserves in the pool)
+ * - m is the amount of token a trader want to sell to us
+ * - A = target_reserve_a, B = target_reserve_b (A/B is the token ratio we want to maintain)
+ * - P = market price, (the number of token b can be purchased by 1 token a)
+ */
 export function calculateOutAmountNormalSwapInternal(
   marketPrice: BigNumber,
   targetReserveA: BigNumber,
@@ -33,6 +41,7 @@ export function calculateOutAmountNormalSwapInternal(
   // round up the float value of core^exp
   let coreExpNumber = Math.pow(coreNumber, expNumber) + 0.00000000000000006;
 
+  // need to ceil the coreExp
   let coreExp: BigNumber = BigNumberWithConfig(currentResreveB.toNumber(), {
     ROUNDING_MODE: BigNumber.ROUND_CEIL,
   }).multipliedBy(new BigNumber(coreExpNumber));
@@ -40,6 +49,12 @@ export function calculateOutAmountNormalSwapInternal(
   return currentResreveB.minus(coreExp);
 }
 
+/**
+ * get the maximum value between the approximation result and calculation using 
+ * calculate_out_amount_normal_swap_internal
+ * both approximation and calculation results are guaranteed to be less or equal to
+ * the theoretical value. we take max of them to get a closer value to the ideal result
+ */
 export function calculateOutAmountNormalSwap(
   marketPrice: BigNumber,
   targetReserveA: BigNumber,
@@ -59,98 +74,4 @@ export function calculateOutAmountNormalSwap(
       inputAAmount,
     ).toNumber(),
   );
-}
-
-export function calculateBalancedReservesStableSwap(
-  stablePrice: BigNumber,
-  currentReserveA: BigNumber,
-  currentResreveB: BigNumber,
-  slope: BigNumber,
-): { balancedReserveA: BigNumber; balancedReserveB: BigNumber } {
-  let coefA: BigNumber = new BigNumber(2).minus(slope).multipliedBy(stablePrice);
-  let coefBNeg: BigNumber = new BigNumber(1)
-    .minus(slope)
-    .multipliedBy(currentReserveA.multipliedBy(stablePrice).plus(currentResreveB));
-  let coefCNeg: BigNumber = slope.multipliedBy(currentReserveA).multipliedBy(currentResreveB);
-  // need to ceil the sqrt
-  let core: BigNumber = BigNumberWithConfig(
-    coefBNeg
-      .multipliedBy(coefBNeg)
-      .plus(coefA.multipliedBy(coefCNeg).multipliedBy(new BigNumber(4))),
-    {
-      ROUNDING_MODE: BigNumber.ROUND_CEIL,
-    },
-  ).squareRoot();
-
-  // need to ceil the div
-  let balancedReserveA: BigNumber = BigNumberWithConfig(coefBNeg.plus(core), {
-    ROUNDING_MODE: BigNumber.ROUND_CEIL,
-  })
-    .dividedBy(coefA)
-    .dividedBy(new BigNumber(2));
-  let balancedReserveB: BigNumber = balancedReserveA.multipliedBy(stablePrice);
-
-  return { balancedReserveA, balancedReserveB };
-}
-
-export function calculateOutAmountStableSwapInternal(
-  balancedReserveA: BigNumber,
-  balancedReserveB: BigNumber,
-  currentReserveA: BigNumber,
-  currentResreveB: BigNumber,
-  inputAAmount: BigNumber,
-  slope: BigNumber,
-): BigNumber {
-  let multiplicand: BigNumber = BigNumberWithConfig(
-    balancedReserveB.multipliedBy(new BigNumber(1).minus(slope)),
-    {
-      ROUNDING_MODE: BigNumber.ROUND_FLOOR,
-    },
-  )
-    .dividedBy(slope)
-    .plus(currentResreveB);
-
-  let coreNumerator: BigNumber = new BigNumber(1)
-    .minus(slope)
-    .multipliedBy(balancedReserveA)
-    .plus(slope.multipliedBy(currentReserveA));
-
-  let coreDenumerator: BigNumber = new BigNumber(1)
-    .minus(slope)
-    .multipliedBy(balancedReserveA)
-    .plus(slope.multipliedBy(currentReserveA.plus(inputAAmount)));
-
-  let multiplier: BigNumber = new BigNumber(1).minus(
-    BigNumberWithConfig(coreNumerator, {
-      ROUNDING_MODE: BigNumber.ROUND_FLOOR,
-    }).dividedBy(coreDenumerator),
-  );
-
-  return multiplicand.multipliedBy(multiplier);
-}
-
-export function calculateOutAmountStableSwap(
-  stablePrice: BigNumber,
-  currentReserveA: BigNumber,
-  currentReserveB: BigNumber,
-  inputAAmount: BigNumber,
-  slope: BigNumber,
-): number {
-  let { balancedReserveA, balancedReserveB } = calculateBalancedReservesStableSwap(
-    stablePrice,
-    currentReserveA,
-    currentReserveB,
-    slope,
-  );
-
-  let result: BigNumber = calculateOutAmountStableSwapInternal(
-    balancedReserveA,
-    balancedReserveB,
-    currentReserveA,
-    currentReserveB,
-    inputAAmount,
-    slope,
-  );
-
-  return Math.floor(result.toNumber());
 }
