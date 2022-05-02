c7bdb9134c5b1c165fb6321ecbd9b122548bc8b9
leqiang-deltafi <leqiang@deltafi.ai>
2022-05-02 15:25:58 -0700

    x

--- tests/utils/swap.test.ts
+++ tests/utils/swap.test.ts
@@ -5,4 +5,24 @@ import {
   getSwapOutAmountSellQuote,
   getSwapOutAmount,
 } from "../../src/utils/swap";
+
+import { Fees } from "../../src/lib/state";
+
 import BigNumber from "bignumber.js";
+
+describe("utils/swap", function () {
+  it("calculatePriceImpact", function () {
+    calculatePriceImpact(
+      new BigNumber(100_000),
+      new BigNumber(200_000),
+      new BigNumber(13),
+      new BigNumber(27.5),
+      new BigNumber(0.5),
+      {
+        adminTradeFeeNumerator: BigInt(1),
+        adminTradeFeeDenominator: BigInt(2),
+      } as Fees
+    );
+    
+  });
+});
