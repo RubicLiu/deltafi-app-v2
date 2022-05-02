b1667364e1ad29c750f2a5c018aed6b3129e8ea3
leqiang-deltafi <leqiang@deltafi.ai>
2022-05-02 10:53:00 -0700

    x

--- src/states/v2/pythV2State.ts
+++ src/states/v2/pythV2State.ts
@@ -5,6 +5,7 @@ import BigNumber from "bignumber.js";
 
 import { getMultipleAccounts } from "utils/account";
 import { deployConfigV2 } from "constants/deployConfigV2";
+import assert from "assert";
 
 type PythData = {
   symbol: string;
@@ -65,7 +66,7 @@ export function getPythPriceBySymbol(
   symbolToPythData: SymbolToPythData,
   tokenSymbol: string | null | undefined,
 ) {
-  let result = { priceData: null, priceAccountKey: null, price: null };
+  let result = { priceData: null, priceAccountKey: null, price: null, confidenceInterval: null };
   if (!tokenSymbol) {
     return result;
   }
@@ -78,16 +79,37 @@ export function getPythPriceBySymbol(
   result.priceData = priceData;
   result.priceAccountKey = symbolToPythData[tokenSymbol].priceKey;
   result.price = priceData.price ? priceData.price : priceData.previousPrice;
+  result.confidenceInterval = priceData.price ? priceData.confidence : priceData.previousConfidence;
+
+  // if the price and confidence interval are both not undefined, price should be greater than confidence interval
+  assert(!(result.price && result.confidenceInterval) || result.price > result.confidenceInterval);
   return result;
 }
 
 export function getPythMarketPrice(symbolToPythData: SymbolToPythData, poolInfo) {
-  const { price: basePrice } = getPythPriceBySymbol(symbolToPythData, poolInfo.base);
-  const { price: quotePrice } = getPythPriceBySymbol(symbolToPythData, poolInfo.quote);
+  const { price: basePrice, confidenceInterval: baseConfidenceInterval } = getPythPriceBySymbol(
+    symbolToPythData,
+    poolInfo.base,
+  );
+  const { price: quotePrice, confidenceInterval: quoteConfidenceInterval } = getPythPriceBySymbol(
+    symbolToPythData,
+    poolInfo.quote,
+  );
   const marketPrice =
     basePrice && quotePrice ? new BigNumber(basePrice / quotePrice) : new BigNumber(NaN);
+  const marketPriceLow =
+    basePrice && quotePrice && baseConfidenceInterval && quoteConfidenceInterval
+      ? new BigNumber((basePrice - baseConfidenceInterval) / (quotePrice + quoteConfidenceInterval))
+      : new BigNumber(NaN);
+  const marketPriceHigh =
+    basePrice && quotePrice && baseConfidenceInterval && quoteConfidenceInterval
+      ? new BigNumber((basePrice + baseConfidenceInterval) / (quotePrice - quoteConfidenceInterval))
+      : new BigNumber(NaN);
+
   return {
     marketPrice,
+    marketPriceHigh,
+    marketPriceLow,
     basePrice,
     quotePrice,
   };
