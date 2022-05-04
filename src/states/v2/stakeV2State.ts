8c9c2c07027a5aed4a5c902918f9f9a311161df4
ypeng-deltafi <ypeng@deltafi.ai>
2022-05-04 12:23:35 -0700

    [PROD-661] Add stake redux state (#26)

---
+++ src/states/v2/stakeV2State.ts
@@ -0,0 +1,76 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import BigNumber from "bignumber.js";
+import { PoolConfig } from "constants/deployConfigV2";
+import { StakeCard } from "views/Stake/components/types";
+
+interface TransactionResult {
+  status: boolean | null;
+  action?: "deposit" | "withdraw";
+  hash?: string;
+  stake?: StakeCard;
+}
+
+const initialState = {
+  isProcessingStake: false,
+  isProcessingClaim: false,
+  transactionResult: null,
+  openSnackbar: false,
+  // TODO(ypeng): Update stake card content to match v2
+  stake: {
+    poolConfig: null,
+    isStake: true,
+    token: null,
+    balance: new BigNumber("0"),
+    amount: "0",
+    percentage: 0,
+  },
+};
+
+const stakeV2Slice = createSlice({
+  name: "stakeV2",
+  initialState,
+  reducers: {
+    setPoolConfig(state, action: PayloadAction<{ poolConfig: PoolConfig }>) {
+      state.stake = {
+        poolConfig: action.payload.poolConfig,
+        isStake: true,
+        token: null,
+        balance: new BigNumber("0"),
+        amount: "0",
+        percentage: 0,
+      };
+    },
+
+    setPercentage(state, action: PayloadAction<{ percentage: number; amount: string }>) {
+      state.stake.percentage = action.payload.percentage;
+      state.stake.amount = action.payload.amount;
+    },
+
+    setBalance(state, action: PayloadAction<{ balance: BigNumber }>) {
+      state.stake.balance = action.payload.balance;
+    },
+
+    setIsStake(state, action: PayloadAction<{ isStake: boolean }>) {
+      state.stake.isStake = action.payload.isStake;
+    },
+
+    setTransactionResult(state, action: PayloadAction<{ transactionResult: TransactionResult }>) {
+      state.transactionResult = action.payload.transactionResult;
+    },
+
+    setIsProcessingStake(state, action: PayloadAction<{ isProcessingStake: boolean }>) {
+      state.isProcessingStake = action.payload.isProcessingStake;
+    },
+
+    setIsProcessingClaim(state, action: PayloadAction<{ isProcessingClaim: boolean }>) {
+      state.isProcessingStake = action.payload.isProcessingClaim;
+    },
+
+    setOpenSnackbar(state, action: PayloadAction<{ openSnackbar: boolean }>) {
+      state.openSnackbar = action.payload.openSnackbar;
+    },
+  },
+});
+
+export const stakeV2Reducer = stakeV2Slice.reducer;
+export const stakeV2Actions = stakeV2Slice.actions;
