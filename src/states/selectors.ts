fee7f83cb7f77e3406cb5ca4fff05e25444fa0b0
Yu Peng <ypeng@deltafi.ai>
2022-05-03 19:46:57 -0700

    x

--- src/states/selectors.ts
+++ src/states/selectors.ts
@@ -7,12 +7,3 @@ export const poolSelector = (state: RootState) => state.pool;
 export const pythSelector = (state: RootState) => state.pyth;
 export const serumSelector = (state: RootState) => state.serum;
 export const tokenAccountSelector = (state: RootState) => state.tokenAccount;
-
-export function selectTokenAccountInfoByMint(mint: string) {
-  return (state: RootState) => {
-    if (state.tokenAccount.mintToTokenAccountInfo == null) {
-      return null;
-    }
-    return state.tokenAccount.mintToTokenAccountInfo[mint];
-  };
-}
