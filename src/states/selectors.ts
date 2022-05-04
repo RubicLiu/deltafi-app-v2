dd9cc6e9db780e1d1fd57938fd9ea3a03f0c21e2
Yu Peng <ypeng@deltafi.ai>
2022-05-03 19:45:43 -0700

    x

--- src/states/selectors.ts
+++ src/states/selectors.ts
@@ -1,11 +1,5 @@
 import { RootState } from "./store";
 
-import { PoolInfo } from "providers/types";
-import { getPoolConfigBySymbols } from "constants/deployConfig";
-import { getPythMarketPrice, getPythPriceBySymbol } from "./pythState";
-import { getserumMarketPrice } from "./serumState";
-import { OraclePriority } from "lib/state";
-
 export const appSelector = (state: RootState) => state.app;
 export const farmUserSelector = (state: RootState) => state.farmUser;
 export const farmPoolSelector = (state: RootState) => state.farmPool;
@@ -13,31 +7,6 @@ export const poolSelector = (state: RootState) => state.pool;
 export const pythSelector = (state: RootState) => state.pyth;
 export const serumSelector = (state: RootState) => state.serum;
 export const tokenAccountSelector = (state: RootState) => state.tokenAccount;
-
-export function getMarketPrice(symbolToPythData, poolNameToSerumPrice, pool) {
-  if (pool?.oraclePriority === OraclePriority.PYTH_ONLY) {
-    return getPythMarketPrice(symbolToPythData, pool);
-  } else {
-    const { price: quotePrice } = getPythPriceBySymbol(
-      symbolToPythData,
-      pool?.quoteTokenInfo.symbol,
-    );
-    return getserumMarketPrice(poolNameToSerumPrice, pool, quotePrice);
-  }
-}
-
-export function selectMarketPriceByPool(pool: PoolInfo) {
-  return (state: RootState) => {
-    return getMarketPrice(state.pyth.symbolToPythData, state.serum.poolNameToSerumPrice, pool);
-  };
-}
-
-export function selectPoolBySymbols(baseSymbol: string, quoteSymbol: string) {
-  return (state: RootState) => {
-    const poolConfig = getPoolConfigBySymbols(baseSymbol, quoteSymbol);
-    return state.pool.poolKeyToPoolInfo[poolConfig?.swap];
-  };
-}
 
 export function selectTokenAccountInfoByMint(mint: string) {
   return (state: RootState) => {
