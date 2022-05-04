2e01ca7d18a74cb231920e73b7c36f92c81c2fb4
Yu Peng <ypeng@deltafi.ai>
2022-05-03 19:48:07 -0700

    x

--- src/states/selectors.ts
+++ src/states/selectors.ts
@@ -1,9 +1,3 @@
 import { RootState } from "./store";
 
 export const appSelector = (state: RootState) => state.app;
-export const farmUserSelector = (state: RootState) => state.farmUser;
-export const farmPoolSelector = (state: RootState) => state.farmPool;
-export const poolSelector = (state: RootState) => state.pool;
-export const pythSelector = (state: RootState) => state.pyth;
-export const serumSelector = (state: RootState) => state.serum;
-export const tokenAccountSelector = (state: RootState) => state.tokenAccount;
