aa44ae16f11f0e1a31676812c799a50ac4f1c2d1
Yu Peng <ypeng@deltafi.ai>
2022-05-03 19:45:05 -0700

    x

--- src/states/v2/selectorsV2.ts
+++ src/states/v2/selectorsV2.ts
@@ -1,7 +1,7 @@
 import { RootState } from "../store";
 
 import { getPythMarketPrice } from "./pythV2State";
-import { getPoolConfigBySymbols } from "constants/deployConfigV2";
+import { getPoolConfigBySymbols, PoolConfig } from "constants/deployConfigV2";
 
 export const lpUserSelector = (state: RootState) => state.liquidityProviderV2;
 export const farmPoolSelector = (state: RootState) => state.farmV2;
@@ -10,9 +10,9 @@ export const pythSelector = (state: RootState) => state.pythV2;
 export const tokenAccountSelector = (state: RootState) => state.tokenV2;
 export const depositSelector = (state: RootState) => state.depositV2;
 
-export function selectMarketPriceByPool(pool) {
+export function selectMarketPriceByPool(poolConfig: PoolConfig) {
   return (state: RootState) => {
-    return getPythMarketPrice(state.pythV2.symbolToPythData, pool);
+    return getPythMarketPrice(state.pythV2.symbolToPythData, poolConfig);
   };
 }
 
