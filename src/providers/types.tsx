69e40f018c5611af5a2c284f1e92672d16305297
Yu Peng <ypeng@deltafi.ai>
2022-05-06 11:45:06 -0700

    x

--- src/providers/types.tsx
+++ src/providers/types.tsx
@@ -1,8 +1,4 @@
-import BigNumber from "bignumber.js";
 import { PublicKey } from "@solana/web3.js";
-
-import { Fees, PoolState, Rewards, SwapType } from "lib/state";
-import { TokenConfig } from "constants/deployConfig";
 
 export interface ConnectionContextValues {
   endpoint: string;
@@ -33,47 +29,4 @@ export interface MarketConfig {
   bumpSeed: number;
   deltafiMint: PublicKey;
   deltafiToken: PublicKey;
-}
-
-export interface PoolInfo {
-  name: string;
-  swapType: SwapType;
-  publicKey: PublicKey;
-  nonce: number;
-  isPaused: boolean;
-  baseTokenInfo: TokenConfig;
-  quoteTokenInfo: TokenConfig;
-  base: PublicKey;
-  quote: PublicKey;
-  pythBase: PublicKey;
-  pythQuote: PublicKey;
-  poolMintKey: PublicKey;
-  baseFee: PublicKey;
-  quoteFee: PublicKey;
-  fees: Fees;
-  rewards: Rewards;
-  poolState: PoolState;
-  oraclePriority: number;
-}
-
-export interface FarmPoolInfo {
-  name: string;
-  publicKey: PublicKey;
-  bumpSeed: number;
-  poolAddress: PublicKey;
-  poolMintKey: PublicKey;
-  poolToken: PublicKey;
-  baseTokenInfo: TokenConfig;
-  quoteTokenInfo: TokenConfig;
-  reservedAmount: bigint;
-  aprNumerator: bigint;
-  aprDenominator: bigint;
-}
-
-export interface StakeAccount {
-  depositBalance: BigNumber;
-  rewardsOwed: BigNumber;
-  rewardEstimated: BigNumber;
-  lastUpdateTs: BigNumber;
-  nextClaimTs: BigNumber;
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,3 @@
-import { PoolInfo } from "providers/types";
-
 export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -18,10 +16,4 @@ export function scheduleWithInterval(func: () => void, delay: number) {
   func();
   const interval = setInterval(func, delay);
   return () => clearInterval(interval);
-}
-
-export function getTokenPairByPoolName(pool: PoolInfo) {
-  return pool.name.startsWith(pool.baseTokenInfo.symbol)
-    ? [pool.baseTokenInfo, pool.quoteTokenInfo]
-    : [pool.quoteTokenInfo, pool.baseTokenInfo];
 }
