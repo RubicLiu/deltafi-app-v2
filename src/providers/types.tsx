cc5f944cf83078dbec1f591a6e2e047285413af4
Yu Peng <ypeng@deltafi.ai>
2022-05-06 15:43:10 -0700

    x

--- src/providers/types.tsx
+++ src/providers/types.tsx
@@ -1,17 +1,5 @@
 import { PublicKey } from "@solana/web3.js";
 
-export interface ConnectionContextValues {
-  endpoint: string;
-  network: string;
-  setNetwork: (network: string) => void;
-}
-
-export interface EndpointInfo {
-  name: string;
-  endpoint: string;
-  custom: boolean;
-}
-
 export interface ModalInfo {
   menuOpen: boolean;
   menu: string;
@@ -22,11 +10,4 @@ export interface ModalInfo {
 export interface ModalContextInfo {
   modalInfo: ModalInfo;
   setMenu: (open: boolean, menu?: string, address?: PublicKey, data?: any) => void;
-}
-
-export interface MarketConfig {
-  publicKey: PublicKey;
-  bumpSeed: number;
-  deltafiMint: PublicKey;
-  deltafiToken: PublicKey;
 }
