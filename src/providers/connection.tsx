3c332ee68b00ad255d0a9c4e2088538024f6b09e
Yu Peng <ypeng@deltafi.ai>
2022-05-06 15:39:04 -0700

    x

--- src/providers/connection.tsx
+++
@@ -1,8 +0,0 @@
-import { ConnectionProvider } from "@solana/wallet-adapter-react";
-import { getClusterApiUrl } from "anchor/anchor_utils";
-import { deployConfigV2 } from "constants/deployConfigV2";
-
-export function CustomConnectionProvider({ children }) {
-  const clusterApiUrl = getClusterApiUrl(deployConfigV2.network);
-  return <ConnectionProvider endpoint={clusterApiUrl}>{children}</ConnectionProvider>;
-}
