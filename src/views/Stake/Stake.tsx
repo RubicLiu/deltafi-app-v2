eb263adad1623c0129758c048ad9542db0506e36
Yu Peng <ypeng@deltafi.ai>
2022-05-09 13:48:56 -0700

    x

--- src/views/Stake/Stake.tsx
+++ src/views/Stake/Stake.tsx
@@ -569,7 +569,7 @@ const Stake = (): ReactElement => {
                 data-amp-analytics-name="click"
                 data-amp-analytics-attrs="page: Deposit, target: Deposit"
               >
-                {staking.isStake ? "Stake" : "Unstake"}
+                {"Stake"}
               </ConnectButton>
             ) : (
               <ConnectButton size="large" fullWidth onClick={() => setMenu(true, "connect")}>
