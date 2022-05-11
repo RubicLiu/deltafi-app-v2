b805475e8d24cfba47b79fdad5be0c4ac860cb10
leqiang-deltafi <leqiang@deltafi.ai>
2022-05-11 16:23:15 -0700

    x

--- src/views/Deposit/Deposit.tsx
+++ src/views/Deposit/Deposit.tsx
@@ -785,7 +785,7 @@ const Deposit: React.FC = () => {
       <Container className={classes.container}>
         <Box display="flex" justifyContent="space-between" className={classes.header}>
           <Typography variant="h6" color="primary">
-            {`${baseTokenInfo.name}-${quoteTokenInfo.name}`}
+            {`${baseTokenInfo.symbol}-${quoteTokenInfo.symbol}`}
           </Typography>
           <Box className={classes.iconGroup}>
             <img
