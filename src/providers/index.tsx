297670522bf1c3b92eaabd0527d86994fa317006
Yu Peng <ypeng@deltafi.ai>
2022-05-16 16:39:53 -0700

    x

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -8,7 +8,8 @@ import { deployConfigV2 } from "constants/deployConfigV2";
 import { ConnectionProvider } from "@solana/wallet-adapter-react";
 
 export function Providers({ children }) {
-  const clusterApiUrl = "https://thrumming-withered-morning.solana-mainnet.quiknode.pro/8e9c62d27589d0c7d7fcb56963125ed8bafb5795/";
+  const clusterApiUrl =
+    "https://thrumming-withered-morning.solana-mainnet.quiknode.pro/8e9c62d27589d0c7d7fcb56963125ed8bafb5795/";
   return (
     <ConnectionProvider endpoint={clusterApiUrl}>
       <WalletProvider>
