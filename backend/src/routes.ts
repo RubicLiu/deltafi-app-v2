abba2715ada1f2a3a20da906f8c509e6cc342a8d
Yu Peng <ypeng@deltafi.ai>
2022-05-23 14:47:53 -0700

    x

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import * as fs from 'fs';
+
+
+const config = JSON.parse(fs.readFileSync('./fullDeployConfigV2.json'))['mainnet-test'];
 
 const routes = Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+  return res.json(config.poolInfoList);
 });
 
 export default routes;
