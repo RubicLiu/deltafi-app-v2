d0d701232af2cd17bc09152a7ea094fca621a1c6
Yu Peng <ypeng@deltafi.ai>
2022-05-23 14:31:27 -0700

    x

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 'use strict';
 
 const express = require('express');
+const fs = require('fs');
 
 const PORT = 4000;
 const HOST = '0.0.0.0';
 
+const config = JSON.parse(fs.readFileSync('./fullDeployConfigV2.json'))['mainnet-test'];
+
 const app = express();
 app.get('/pools', (_, response) => {
-  response.json([
-    {
-      date: new Date().toISOString(),
-    },
-  ]);
+  response.json(config.poolInfoList);
 });
 
 app.listen(PORT, HOST);
