b966e86a0175c1ca8b1da0eac4fa3d8eb34b00bc
Yu Peng <ypeng@deltafi.ai>
2022-05-23 21:53:59 -0700

    x

--- deploy/build_dev.sh
+++ deploy/build_dev.sh
@@ -8,4 +8,4 @@ export REGISTRY=077918681028.dkr.ecr.us-west-2.amazonaws.com/deltafi-dev
 export TAG=`git rev-parse --short HEAD`-`date +%Y%m%d-%H%M%S`
 $SCRIPT_DIR/build_docker.sh
 
-echo $TAG
+echo $TAG > $SCRIPT_DIR/dev/tag.txt
