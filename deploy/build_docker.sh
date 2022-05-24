7b60a841f6d287c1c88ab713a955c997a00bf6bb
Yu Peng <ypeng@deltafi.ai>
2022-05-23 21:52:41 -0700

    x

--- deploy/build_docker.sh
+++ deploy/build_docker.sh
@@ -7,9 +7,6 @@ cd $SCRIPT_DIR/..
 
 cp src/anchor/fullDeployConfigV2.json backend/src
 
-yarn install
-yarn build-dev
-
 REPO=deltafi-app-v2/frontend
 docker build -t ${REPO}:${TAG} --platform linux/amd64 .
 docker tag ${REPO}:${TAG} ${REGISTRY}/${REPO}:${TAG}
