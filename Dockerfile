948877b4d5853aa43477a9978ab3e9f5454cb37c
Yu Peng <ypeng@deltafi.ai>
2022-05-23 17:09:22 -0700

    x

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,11 @@ FROM node:16 as build
 WORKDIR /usr/app
 
 ADD package.json tsconfig.json yarn.lock /usr/app
+RUN yarn install
+
 ADD src /usr/app/src
 ADD public /usr/app/public
-RUN yarn install && yarn build-dev
+RUN yarn build-dev
 
 FROM nginx:latest
 COPY --from=build /usr/app/build /usr/share/nginx/html
--- build_docker.sh
+++ build_docker.sh
@@ -3,9 +3,14 @@
 set -e -x
 
 REGISTRY=077918681028.dkr.ecr.us-west-2.amazonaws.com/deltafi-dev
-REPO=deltafi-app-v2/backend
-TAG=backend-test
+TAG=`git rev-parse --short HEAD`-`date +%Y%m%d-%H%M%S`
 
+REPO=deltafi-app-v2/frontend
+docker build -t ${REPO}:${TAG} --platform linux/amd64 .
+docker tag ${REPO}:${TAG} ${REGISTRY}/${REPO}:${TAG}
+docker push ${REGISTRY}/${REPO}:${TAG}
+
+REPO=deltafi-app-v2/backend
 docker build -t ${REPO}:${TAG} --platform linux/amd64 backend
 docker tag ${REPO}:${TAG} ${REGISTRY}/${REPO}:${TAG}
 docker push ${REGISTRY}/${REPO}:${TAG}
