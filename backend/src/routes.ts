c1248347cb6e926cb1d42b9bd3eccb76b40de322
Yu Peng <ypeng@deltafi.ai>
2022-05-24 14:46:16 -0700

    x

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import * as fs from 'fs';
+import * as GateApi from "gate-api";
 
 import fullDeployConfigV2 from "./fullDeployConfigV2.json";
 
 const config = fullDeployConfigV2['mainnet-test'];
+const gateApiClient = new GateApi.ApiClient();
 
 const routes = Router();
 
@@ -12,11 +13,17 @@ routes.get('/pools', (_, response) => {
 });
 
 routes.get('/spot/tickers/:currencyPair', (request, response) => {
-  
-
-  return response.json({
-    currencyPair: request.params.currencyPair
-  });
+  const api = new GateApi.SpotApi(gateApiClient);
+  const currencyPair = request.params.currencyPair;
+  api.listTickers({ currencyPair })
+   .then(
+     value => {
+      console.log('API called successfully. Returned data: ', value.body);
+      response.json(value.body)
+    },
+    error => {
+      console.error(error);
+    });
 });
 
 export default routes;
