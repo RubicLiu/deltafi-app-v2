aaa8a8cb4f127b93b27d5dac68fcd392c01cd4b5
ypeng-deltafi <ypeng@deltafi.ai>
2022-05-25 09:47:08 -0700

    [PROD-704] Add in-memory cache for gate io proxy (#80)

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { CacheContainer } from 'node-ts-cache';
+import { MemoryStorage } from 'node-ts-cache-storage-memory';
 import * as GateApi from "gate-api";
 
 import fullDeployConfigV2 from "./fullDeployConfigV2.json";
@@ -7,16 +9,26 @@ const config = fullDeployConfigV2['mainnet-test'];
 const gateApiClient = new GateApi.ApiClient();
 const spotApi = new GateApi.SpotApi(gateApiClient);
 
+const cacheTtlInSecs = 60;
+const tickersCache = new CacheContainer(new MemoryStorage())
+const candlesticksCache = new CacheContainer(new MemoryStorage())
+
 const routes = Router();
 
-routes.get('/pools', (_, response) => {
+routes.get('/api/pools', (_, response) => {
   return response.json(config.poolInfoList);
 });
 
-routes.get('/spot/tickers/:currencyPair', async (request, response) => {
+routes.get('/api/spot/tickers/:currencyPair', async (request, response) => {
   const currencyPair = request.params.currencyPair;
+  const cachedTickers = await tickersCache.getItem(currencyPair);
+  if (cachedTickers) {
+    return response.json(cachedTickers);
+  }
+
   try {
     const result = await spotApi.listTickers({ currencyPair });
+    tickersCache.setItem(currencyPair, result.body, {ttl: cacheTtlInSecs});
     return response.json(result.body);
   } catch (e) {
     console.error(e);
@@ -24,12 +36,18 @@ routes.get('/spot/tickers/:currencyPair', async (request, response) => {
   }
 });
 
-routes.get('/spot/candlesticks/:currencyPair', async (request, response) => {
+routes.get('/api/spot/candlesticks/:currencyPair', async (request, response) => {
   const currencyPair = request.params.currencyPair;
+  const cachedCandleSticks = await candlesticksCache.getItem(currencyPair);
+  if (cachedCandleSticks) {
+    return response.json(cachedCandleSticks);
+  }
+
   try {
     const result = await spotApi.listCandlesticks(currencyPair, {
       interval: "30m",
     });
+    candlesticksCache.setItem(currencyPair, result.body, {ttl: cacheTtlInSecs});
     return response.json(result.body);
   } catch (e) {
     console.error(e);
