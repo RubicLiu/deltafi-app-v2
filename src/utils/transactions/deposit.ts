89ed52e831cf72017d84258f32295df440c8899b
Yu Peng <ypeng@deltafi.ai>
2022-05-26 10:42:24 -0700

    x

--- src/utils/transactions/deposit.ts
+++ src/utils/transactions/deposit.ts
@@ -246,43 +246,43 @@ export async function createWithdrawTransaction(
   });
 }
 
-export async function createClaimFarmRewardsTransaction(
-  program: any,
-  connection: Connection,
-  poolConfig: PoolConfig,
-  walletPubkey: PublicKey,
-  userDeltafiToken: PublicKey,
-) {
-  const [lpPublicKey] = await PublicKey.findProgramAddress(
-    [
-      Buffer.from("LiquidityProvider"),
-      new PublicKey(poolConfig.swapInfo).toBuffer(),
-      walletPubkey.toBuffer(),
-    ],
-    program.programId,
-  );
-
-  let transaction = new Transaction();
-  transaction.add(
-    program.transaction.claimFarmRewards({
-      accounts: {
-        marketConfig: new PublicKey(deployConfigV2.marketConfig),
-        farmInfo: new PublicKey(poolConfig.farmInfo),
-        swapInfo: new PublicKey(poolConfig.swapInfo),
-        liquidityProvider: lpPublicKey,
-        userDeltafiToken,
-        swapDeltafiToken: new PublicKey(deployConfigV2.deltafiToken),
-        owner: walletPubkey,
-        tokenProgram: token.TOKEN_PROGRAM_ID,
-      },
-    }),
-  );
-
-  const signers = [];
-  return partialSignTransaction({
-    transaction,
-    feePayer: walletPubkey,
-    signers,
-    connection,
-  });
-}
+//export async function createClaimFarmRewardsTransaction(
+//  program: any,
+//  connection: Connection,
+//  poolConfig: PoolConfig,
+//  walletPubkey: PublicKey,
+//  userDeltafiToken: PublicKey,
+//) {
+//  const [lpPublicKey] = await PublicKey.findProgramAddress(
+//    [
+//      Buffer.from("LiquidityProvider"),
+//      new PublicKey(poolConfig.swapInfo).toBuffer(),
+//      walletPubkey.toBuffer(),
+//    ],
+//    program.programId,
+//  );
+//
+//  let transaction = new Transaction();
+//  transaction.add(
+//    program.transaction.claimFarmRewards({
+//      accounts: {
+//        marketConfig: new PublicKey(deployConfigV2.marketConfig),
+//        farmInfo: new PublicKey(poolConfig.farmInfo),
+//        swapInfo: new PublicKey(poolConfig.swapInfo),
+//        liquidityProvider: lpPublicKey,
+//        userDeltafiToken,
+//        swapDeltafiToken: new PublicKey(deployConfigV2.deltafiToken),
+//        owner: walletPubkey,
+//        tokenProgram: token.TOKEN_PROGRAM_ID,
+//      },
+//    }),
+//  );
+//
+//  const signers = [];
+//  return partialSignTransaction({
+//    transaction,
+//    feePayer: walletPubkey,
+//    signers,
+//    connection,
+//  });
+//}
