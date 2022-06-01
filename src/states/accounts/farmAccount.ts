6cba9d94c5d3122e96aaa9189cd62e3a1914e9d1
leqiang-deltafi <96594219+leqiang-deltafi@users.noreply.github.com>
2022-06-01 10:08:48 -0700

    [PROD-731] Add Farms and FarmUser account states (#102)

---
+++ src/states/accounts/farmAccount.ts
@@ -0,0 +1,47 @@
+import { createReducer, createAsyncThunk } from "@reduxjs/toolkit";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { deployConfigV2 } from "constants/deployConfigV2";
+import { getDeltafiDexV2, makeProvider } from "anchor/anchor_utils";
+
+const initialState = {
+  farmKeyToFarmInfo: {},
+};
+
+type FetchFarmsThunkArg = {
+  connection: Connection;
+};
+
+// TODO: need to test after farm transactions are added
+export const fetchFarmsThunk = createAsyncThunk(
+  "v2/fetchFarms",
+  async (arg: FetchFarmsThunkArg) => {
+    const program = getDeltafiDexV2(
+      new PublicKey(deployConfigV2.programId),
+      makeProvider(arg.connection, {}),
+    );
+
+    const poolInfoList = deployConfigV2.poolInfoList;
+    const farmAddressList = poolInfoList
+      ?.map(({ farmInfoList }) => farmInfoList?.map(({ farmInfo }) => new PublicKey(farmInfo)))
+      .flat();
+
+    const farmInfoList = await program.account.farmInfo.fetchMultiple(farmAddressList);
+    const farmKeyToFarmInfo = {};
+
+    for (let i = 0; i < farmAddressList.length; i++) {
+      const farmInfo = farmInfoList[i];
+      const farmInfoAddress = farmAddressList[i];
+      farmKeyToFarmInfo[farmInfoAddress] = farmInfo;
+    }
+
+    return {
+      farmKeyToFarmInfo,
+    };
+  },
+);
+
+export const farmAccountReducer = createReducer(initialState, (builder) => {
+  builder.addCase(fetchFarmsThunk.fulfilled, (state, action) => {
+    state.farmKeyToFarmInfo = action.payload.farmKeyToFarmInfo;
+  });
+});
---
+++ src/states/accounts/farmUserAccount.ts
@@ -0,0 +1,66 @@
+import { createReducer, createAsyncThunk } from "@reduxjs/toolkit";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { deployConfigV2 } from "constants/deployConfigV2";
+import { getDeltafiDexV2, makeProvider } from "anchor/anchor_utils";
+import { FarmUser } from "anchor/type_definitions";
+
+export type FarmPoolKeyToFarmUser = Record<string, FarmUser>;
+
+export interface FarmUserState {
+  farmPoolKeyToFarmUser: FarmPoolKeyToFarmUser;
+}
+
+const initialState: FarmUserState = {
+  farmPoolKeyToFarmUser: {},
+};
+
+type FetchFarmUsersThunkArg = {
+  connection: Connection;
+  walletAddress: PublicKey;
+};
+
+// TODO: need to test after farm transactions are added
+export const fetchFarmUsersThunk = createAsyncThunk(
+  "farmUser/fetchFarmUsers",
+  async (arg: FetchFarmUsersThunkArg) => {
+    const program = getDeltafiDexV2(
+      new PublicKey(deployConfigV2.programId),
+      makeProvider(arg.connection, {}),
+    );
+
+    const poolInfoList = deployConfigV2.poolInfoList;
+    const farmAddressList = poolInfoList
+      .map(({ farmInfoList }) => farmInfoList.map(({ farmInfo }) => new PublicKey(farmInfo)))
+      .flat();
+
+    const farmPoolKeyToFarmUser = {};
+    const farmUserAddressList = [];
+
+    for (let i = 0; i < farmAddressList.length; i++) {
+      const farmInfo = farmAddressList[i];
+      const [farmUserPubKey] = await PublicKey.findProgramAddress(
+        [Buffer.from("FarmUser"), farmInfo.toBuffer(), arg.walletAddress.toBuffer()],
+        program.programId,
+      );
+      farmUserAddressList.push(farmUserPubKey);
+    }
+
+    const farmUserList = await program.account.farmUser.fetchMultiple(farmUserAddressList);
+
+    for (let i = 0; i < farmAddressList.length; i++) {
+      const farmInfoAddress = farmAddressList[i];
+      const farmUser = farmUserList[i];
+      farmPoolKeyToFarmUser[farmInfoAddress] = farmUser;
+    }
+
+    return {
+      farmPoolKeyToFarmUser,
+    };
+  },
+);
+
+export const farmUserReducer = createReducer(initialState, (builder) => {
+  builder.addCase(fetchFarmUsersThunk.fulfilled, (state, action) => {
+    state.farmPoolKeyToFarmUser = action.payload.farmPoolKeyToFarmUser;
+  });
+});
