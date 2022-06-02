a59d4386590ec67e176adc388f05cc5620e57740
Yu Peng <ypeng@deltafi.ai>
2022-06-01 19:01:41 -0700

    x

--- src/anchor/type_definitions.ts
+++ src/anchor/type_definitions.ts
@@ -68,15 +68,13 @@ export interface SwapConfig {
 }
 
 export type SwapDirection =
-| { sellBase? : any, sellQuote?: never }
-| { sellBase?: never, sellQuote? : any }
-
+  | { sellBase?: any; sellQuote?: never }
+  | { sellBase?: never; sellQuote?: any };
 
 export type SwapType =
-| { normalSwap? : any, stableSwap?: never, serumSwap?: never }
-| { normalSwap?: never, stableSwap? : any, serumSwap?: never }
-| { normalSwap?: never, stableSwap?: never, serumSwap? : any }
-
+  | { normalSwap?: any; stableSwap?: never; serumSwap?: never }
+  | { normalSwap?: never; stableSwap?: any; serumSwap?: never }
+  | { normalSwap?: never; stableSwap?: never; serumSwap?: any };
 
 export interface DeltafiUser {
   bump: number;
