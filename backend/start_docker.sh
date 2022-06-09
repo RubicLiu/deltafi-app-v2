8956c04ab6d1f0dde6abdc1a9bf1d699132da6c3
Yu Peng <ypeng@deltafi.ai>
2022-06-08 17:42:07 -0700

    Fix commands in readme

--- backend/start_docker.sh
+++ backend/start_docker.sh
@@ -8,7 +8,8 @@ cd $SCRIPT_DIR
 
 REPO=backend
 TAG=backend-test
-docker build -t ${REPO}:${TAG} .
+BUILD_COMMAND=build
+docker build -t ${REPO}:${TAG} --build-arg build_command=${BUILD_COMMAND} .
 
 docker rm -f $REPO || true
 docker run -t -i --name $REPO -p 4000:4000 ${REPO}:${TAG}
